661715ce0bccccbbd6f70e1cc4d79f95548fb14a
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2024-01-07 14:16:33 +0530

    Initializing Grid

---
+++ src/components/node/Node.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Node = (props: Props) => {
+  return (
+    <div className="h-7 w-7 outline-cyan-300 outline outline-1"></div>
+  )
+}
+
+export default Node
