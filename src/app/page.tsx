7fa4b515b160c6518168cabae18c74e7c335f0fb
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2024-01-07 15:02:35 +0530

    Start node and last node reference point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,32 @@ import Node from '@/components/node/Node'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
+type NodeType = {
+  col: number
+  row: number,
+  isStart: boolean,
+  isFinish: boolean
+}
+
+
+
+
+
 export default function Home() {
-  const [nodes, setNodes] = useState([[[]]])
+  const [nodes, setNodes] = useState<NodeType[][]>([[{ col: 0, row: 0, isStart: false, isFinish: false }]])
 
   const genGrid = () => {
     const gridNode = []
     for (let row = 0; row < 15; row++) {
       const curRow = []
       for (let col = 0; col < 50; col++) {
-        curRow.push([])
+        const curNode: NodeType = {
+          col,
+          row,
+          isStart: row == 5 && col == 5,
+          isFinish: row == 10 && col == 40
+        }
+        curRow.push(curNode)
       }
       gridNode.push(curRow)
     }
@@ -25,9 +42,14 @@ export default function Home() {
     <main className='mx-10 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400'>
       <div className='max-w-max max-h-max'>
         {
-          nodes.map((row, rowIdx) => {
-            return <div className='flex'>
-              {row.map((node, nodeIdx) => <Node/>)}
+          nodes.map((row: NodeType[], rowIdx: number) => {
+
+            return <div className='flex' key={rowIdx}>
+              {row.map((node: NodeType, nodeIdx: number) => {
+                const { isStart, isFinish } = node
+                return (<Node key={nodeIdx} isStart={isStart} isFinish={isFinish} />)
+              }
+              )}
             </div>
           })
         }
@@ -35,3 +57,5 @@ export default function Home() {
     </main>
   )
 }
+
+
--- src/components/node/Node.tsx
+++ src/components/node/Node.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-type Props = {}
 
-const Node = (props: Props) => {
+const Node = ({key,isStart, isFinish}:{key: number, isStart: boolean, isFinish:boolean}) => {
+
+  const nodeDesignClass = isStart ? 'bg-red-600' : isFinish ? 'bg-blue-600' : ''
   return (
-    <div className="h-7 w-7 outline-cyan-300 outline outline-1"></div>
+    <div className={`h-7 w-7 outline-cyan-300 outline outline-1 ${nodeDesignClass}`}></div>
   )
 }
 
