50279b452b63d112ad6bd2c2c3d168288442cd5d
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2024-01-07 15:08:34 +0530

    node point color change for ref

--- src/components/node/Node.tsx
+++ src/components/node/Node.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Node = ({key,isStart, isFinish}:{key: number, isStart: boolean, isFinish:boolean}) => {
 
-  const nodeDesignClass = isStart ? 'bg-red-600' : isFinish ? 'bg-blue-600' : ''
+  const nodeDesignClass = isStart ? 'bg-yellow-400' : isFinish ? 'bg-red-400' : ''
   return (
-    <div className={`h-7 w-7 outline-cyan-300 outline outline-1 ${nodeDesignClass}`}></div>
+    <div key={key} className={`h-7 w-7 outline-cyan-300 outline outline-1 ${nodeDesignClass}`}></div>
   )
 }
 
